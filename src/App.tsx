aa3a32a0c54820fef7a45f74105a3972273cd9d2
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-05-26 23:13:53 +0900

    [add]20210526 super moon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,92 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isNamedTupleMember } from 'typescript';
 
 function App() {
+
+  const isFlag1: boolean = true;
+  const isFlag2: boolean = false;
+  const isFlag3: string = 'aaa';
+  const num1: number = 10;
+  const num2: number = -10;
+  const num3: string = 'aaa';
+
+  const str1: string = 'aaa';
+  const str2: number = -10;
+  const str3: boolean = false
+
+  let temp: string[] = [`adf`, 'kdk', 'iku'];
+  let temp2: Array<string> = [`adf`, 'kdk', 'iku'];
+
+  let taple: [string, number] = ['satake', 999];
+
+  let profile1: any = 12345;
+  let profile2: any = 'test';
+  let profile3: any = false;
+
+  const test = (): string => {
+    return 'Hello'
+  };
+
+  const test1 = (): null => {
+    return null
+  };
+
+  const test2 = (): undefined => {
+    return undefined
+  };
+
+  const error = (number: string): never => {
+    throw new Error();
+  };
+  console.log(error('hello'));
+
+  type TestType = {
+    id: number,
+    name: string
+  }
+  let object1: TestType = { id: 1, name: 'satake' }
+  let object2: { id: number, name: string } = { id: 1, name: 'satake' };
+
+  type ObjectInterface = {
+    id: number;
+    name: string;
+  }
+  let object3: ObjectInterface = { id: 1, name: 'satake' };
+
+  const kansu = (): number => 43;
+  let numberAny: any = kansu();
+  let numberUnknown: number = kansu();
+
+  let sum1 = 10 + numberAny
+  let sum2 = 10 + numberUnknown
+
+
+  type Pitcher1 = {
+    throwingSpeed: number;
+  };
+
+  type Batter1 = {
+    battingAverage: number;
+  };
+
+  type TwoWayPlayer = Pitcher1 & Batter1;
+
+  let sasaki: TwoWayPlayer = {
+    throwingSpeed: 154,
+    battingAverage: 3.65,
+  };
+
+
+  let name: string | number | boolean = 'sasaki'
+  name = 100
+  name = false
+
+  let nitiyoubi: 'sun' = 'sun'
+  let yes: true = true;
+  let week: 'sun' | 'mun' | 'tue' | 'wen' | 'thu' | 'fry' | 'sut' = 'sun';
+
   return (
     <div className="App">
       <header className="App-header">
